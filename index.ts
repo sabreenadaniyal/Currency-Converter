33654c832b8ad1640732d3d57e86aaf16e90630f
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-04-17 02:04:30 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+const Currency:any={USD:1, //Base currency
+                   EUR:0.91,
+                   GBP:0.76,
+                   INR:74.57,
+                   PKR:280,};
+
+let answer = await inquirer.prompt([
+{message:"Enter from currency",name:"from",type:"list",
+choices:["USD","EUR","GBP","INR","PKR"]},
+
+{message:"Enter to currency",name:"to",type:"list",
+choices:["USD","EUR","GBP","INR","PKR"]},
+
+{message:"Enter your amount",name:"amount",type:"number"}
+])
+
+//Formula
+//Currency[answer.to] / Currency[answer.from] * answer.amount
+
+console.log(chalk.greenBright(Currency[answer.to]/Currency[answer.from]*answer.amount));
+
